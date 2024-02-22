3ae5ff4df56df616af224645c8e0ad31eb6f44cd
Calvin Zhao <calvin.zhao12321@gmail.com>
2024-02-22 01:30:08 -0500

    changed url to live

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, {useContext, useState} from "react";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000/api/v1/"
+const BASE_URL = "https://tofinancialfreedom.netlify.app/api/v1/"
 const GlobalContext = React.createContext()
 
 export const GlobalProvider = ({children})=>{
