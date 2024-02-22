917a091f68caefd0dfcb8b6dd7e6c39e01ef1a79
Calvin Zhao <calvin.zhao12321@gmail.com>
2024-02-22 03:10:44 -0500

    .

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -30,8 +30,6 @@ exports.addIncome = async(req,res)=>{
 exports.getIncomes = async(req,res)=>{
     try {
         const income = await IncomeSchema.find().sort({createdAt: -1})
-        console.log("hello")
-        console.log(income)
         res.status(200).json(income)
     } catch (error) {
         res.status(500).json({message: "Server Error"})
